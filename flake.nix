f41d74aa56c2528c46f58977010c6ce99619921a
pcasaretto <pcasaretto@gmail.com>
2022-09-20 10:22:30 -0300

    add devshells to flake.nix

--- flake.nix
+++ flake.nix
@@ -20,35 +20,35 @@
 
     in
     {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+          with pkgs;
+        {
+          default = mkShell {
+            inputsFrom = builtins.attrValues self.packages.${system};
+            buildInputs = [gopls gotools go-tools];
+          };
 
-      # Provide some binary packages for selected system types.
+        });
+
       packages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
         in
+          with pkgs;
         {
-          # The default package for 'nix build'. This makes sense if the
-          # flake provides only one package or there is a clear "main"
-          # package.
-          default = pkgs.buildGoModule {
+          default = buildGoModule {
             pname = "dotenv";
             inherit version;
             # In 'nix develop', we don't need a copy of the source tree
             # in the Nix store.
             src = ./.;
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            #vendorSha256 = pkgs.lib.fakeSha256;
-
             vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
           };
+
         });
     };
 }
